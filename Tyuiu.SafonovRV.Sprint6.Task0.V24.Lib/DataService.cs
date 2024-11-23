0a546031d1b54c291b6d90c4b49e02030bd043d0
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-11-23 19:13:07 +0500

    Task0

--- Tyuiu.SafonovRV.Sprint6.Task0.V24.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint6.Task0.V24.Lib/DataService.cs
@@ -1,15 +1,14 @@
-﻿
-using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.SafonovRV.Sprint6.Task0.V24.Lib
 {
     public class DataService : ISprint6Task0V24
     {
-
-
         public double Calculate(int x)
         {
             return Math.Round(Math.Pow((x - 1), 3 * x + 1), 3);
         }
     }
 }
+    
+
