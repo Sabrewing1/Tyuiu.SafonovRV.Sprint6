5c1b25ccc7b1ca8cb3950928c69d9a0cecb50519
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-12-08 14:10:25 +0500

    Task5

--- Tyuiu.SafonovRV.Sprint6.Task5.V21.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint6.Task5.V21.Lib/DataService.cs
@@ -1,39 +1,36 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.SafonovRV.Sprint6.Task5.V21.Lib
 {
 
     public class DataService : ISprint6Task5V21
     {
-        public int len = 0;
         public double[] LoadFromDataFile(string path)
         {
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null) { len++; }
-            }
+            string[] lines = File.ReadAllLines(path);
+            var numsList = new System.Collections.Generic.List<double>();
 
-            double[] numsArray = new double[len];
-
-            int index = 0;
-            using (StreamReader reader = new StreamReader(path))
+            foreach (var line in lines)
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var part in parts)
                 {
-                    numsArray[index] = Convert.ToDouble(line);
-                    index++;
+                    string normalizedPart = part.Replace(',', '.');
+                    if (double.TryParse(normalizedPart, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                    {
+                        numsList.Add(number);
+                    }
                 }
             }
-
-            numsArray = numsArray.Where(val => val % 5 != 0).ToArray();
-
-            return numsArray;
+            double[] NumsArray = numsList.ToArray();
+            NumsArray = NumsArray.Where(val => val % 3 == 0).ToArray();
+            return NumsArray;
         }
     }
 }
-    
+
 
 
 
