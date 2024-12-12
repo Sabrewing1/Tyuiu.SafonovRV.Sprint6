b8d20a6dea4aaf4ff9e689febee729dcc68c30dd
Tyuiu.SafonovRV@outlook.com <Tyuiu.SafonovRV@outlook.com>
2024-12-12 21:55:06 +0500

    Task1

--- Tyuiu.SafonovRV.Sprint6.Task1.V4.Lib/DataService.cs
+++ Tyuiu.SafonovRV.Sprint6.Task1.V4.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.SafonovRV.Sprint6.Task1.V4.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = Math.Round((Math.Sin(x) / (x + 1.2)) + Math.Sin(x) * 2 - 2*x, 2);
+                y = Math.Round((Math.Sin(x) / (x + 1.2)) - Math.Sin(x) * 2 - 2*x, 2);
                 valueArray[count] = y;
                 count++;
             }
